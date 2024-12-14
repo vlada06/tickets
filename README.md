27b7b0826609100ba306005bea3bc6d4505d9549
Vlada <vladimir.davidovic@autotrader.co.uk>
2024-12-14 17:06:31 +0000

    Implementing the EventController Read only requests

---
+++ README.md
@@ -0,0 +1,12 @@
+# What Have We Learned So Far
+
+### Section 3: Building REST Web Services:
+ - Implementing the EventController
+   - URL cannot be https! Has to be non-secured, http.
+   - IntelliJ has a bit of problem running services. Need to run HTTP REST request as a scratch file
+   - URL used: `GET http://localhost:8078/organizers`
+   - Another way to run a REST request:
+     - in services pane click `+`, add and name request. Theoretically, right-click on the request name should
+
+### Reference Documentation
+ * Further reference, and links can be found in [HELP.md](HELP.md) 
--- src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
+++ src/main/java/com/pluralsight/springboot/tickets/events/EventController.java
@@ -28,19 +28,19 @@ public class EventController {
         return organizerRepository.findAll();
     }
 
-//    @GetMapping(path = "/events")
-//    public List<Event> getEventsByOrganizer(@RequestParam("organizerId") int organizerId) {
-//        return eventRepository.findByOrganizerId(organizerId);
-//    }
+    @GetMapping(path = "/events")
+    public List<Event> getEventsByOrganizer(@RequestParam("organizerId") int organizerId) {
+        return eventRepository.findByOrganizerId(organizerId);
+    }
 
-//    @GetMapping(path = "/events/{id}")
-//    public Event getEventById(@PathVariable("id") int eventId) {
-//        return eventRepository.findById(eventId)
-//                .orElseThrow(() -> new NoSuchElementException("Event with id " + eventId + " not found"));
-//    }
+    @GetMapping(path = "/events/{id}")
+    public Event getEventById(@PathVariable("id") int eventId) {
+        return eventRepository.findById(eventId)
+                .orElseThrow(() -> new NoSuchElementException("Event with id " + eventId + " not found"));
+    }
 
-//    @GetMapping(path = "/products")
-//    public List<Product> getProductsByEvent(@RequestParam("eventId") int eventId) {
-//        return productRepository.findByEventId(eventId);
-//    }
+    @GetMapping(path = "/products")
+    public List<Product> getProductsByEvent(@RequestParam("eventId") int eventId) {
+        return productRepository.findByEventId(eventId);
+    }
 }
