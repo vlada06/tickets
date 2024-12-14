7d5be05b27af8233a877b8023b94172b9de4bae5
Vlada <vladimir.davidovic@autotrader.co.uk>
2024-12-14 18:11:06 +0000

    Implement RegistrationController CRUD operations

--- src/main/java/com/pluralsight/springboot/tickets/registration/RegistrationController.java
+++ src/main/java/com/pluralsight/springboot/tickets/registration/RegistrationController.java
@@ -1,11 +1,41 @@
 package com.pluralsight.springboot.tickets.registration;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/registration")
+@RequestMapping("/registrations")
+//@RequestMapping(path = "/registrations")
 public class RegistrationController {
-//    private final RegRep
 
+    private final RegistrationRepository registrationRepository;
+
+    public RegistrationController(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
+    @PostMapping
+    public Registration create(@RequestBody @Valid Registration registration) {
+        return registrationRepository.create(registration);
+    }
+
+//    @GetMapping(path = "/{ticketCode}")
+    @GetMapping("/{ticketCode}")
+    public Registration get(@PathVariable("ticketCode") String ticketCode) {
+        return registrationRepository.findByTicketCode(ticketCode)
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code " + ticketCode + " not found"));
+    }
+
+    @PutMapping
+    public Registration update(@RequestBody Registration registration) {
+        return registrationRepository.update(registration);
+    }
+
+    @DeleteMapping(path = "/{ticketCode}")
+    public void delete(@PathVariable("ticketCode") String ticketCode) {
+        registrationRepository.deleteByTicketCode(ticketCode);
+    }
 }
+
